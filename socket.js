6c01221b2fabf9c6f4ada06f86feec69b05d3cda
Özgür ÖZALP <ozgurozalp1999@gmail.com>
2019-12-08 15:30:15 +0300

    fixed errors

--- socket.js
+++ socket.js
@@ -3,7 +3,8 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const PORT = process.env.PORT || 5000;
 
-const firsClient = null;
+let firsClient = null;
+let firsClient = null;
 const allClient = [];
 
 app.get("/", (req, res) => {
@@ -12,6 +13,7 @@ app.get("/", (req, res) => {
 
 io.on("connection", socket => {
   if (socket.client.conn.server.clientsCount == 1) firsClient = socket.id;
+  if (socket.client.conn.server.clientsCount == 2) secondClient = socket.id;
 
   allClient.push(socket.id);
 
