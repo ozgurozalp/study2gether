83811d6cb9e718a1cc4cd3c91fcdb0860f04be1b
Özgür ÖZALP <ozgurozalp1999@gmail.com>
2019-12-08 14:18:33 +0300

    ayarlar yapıldı

--- socket.js
+++ socket.js
@@ -2,10 +2,14 @@ const app = require("express")();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const PORT = process.env.PORT || 5000;
+let kullaniciSayisi = 0;
 
 app.get("/", (req, res) => {
   res.send("404");
 });
+
+socket.on("connect", () => kullaniciSayisi++);
+socket.on("disconnect", () => kullaniciSayisi--);
 
 io.on("connection", socket => {
   console.log("sokete bağlananlar var");
@@ -20,9 +24,12 @@ io.on("connection", socket => {
   socket.on("howManyClients", data => {
     console.log(data);
     io.emit("clients", {
-      count: io.sockets.clients().length
+      count: kullaniciSayisi
     });
   });
 });
+
+socket.on("connect", () => kullaniciSayisi++);
+socket.on("disconnect", () => kullaniciSayisi--);
 
 server.listen(PORT, () => console.log(`${PORT} portunda çalışıyorum.`));
