70a03f3e92e814c051c18aa2641f84dbef911ef3
Özgür ÖZALP <ozgurozalp1999@gmail.com>
2019-12-08 15:00:04 +0300

    fixed errors

--- socket.js
+++ socket.js
@@ -3,22 +3,29 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const PORT = process.env.PORT || 5000;
 
+const firsClient = null;
+const allClient = [];
 app.get("/", (req, res) => {
   res.send("404");
 });
 
 io.on("connection", socket => {
-  console.log("sokete bağlananlar var");
+  if (socket.client.conn.server.clientsCount == 1) firsClient = socket.id;
+
+  allClient.push(socket.id);
+
+  let firsClientIndex = allClient.findIndex(id => id == firsClient);
+  if (firsClient > -1) allClient.splice(firsClient, 1);
 
   socket.on("broadcast", data => {
-    console.log(data);
-    io.emit("text", {
-      text: data.text
+    allClient.forEach(e => {
+      e.emit("text", {
+        text: data.text
+      });
     });
   });
 
   socket.on("howManyClients", data => {
-    console.log(data);
     io.emit("clients", {
       count: socket.client.conn.server.clientsCount,
       id: socket.id
