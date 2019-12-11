0af9410c2ffb118a2dca15397bfca7581382d881
Özgür ÖZALP <ozgurozalp1999@gmail.com>
2019-12-11 22:19:10 +0300

    change set header

--- socket.js
+++ socket.js
@@ -46,6 +46,7 @@ io.on("connection", socket => {
 	});
 
 	app.get("/", (req, res) => {
+		res.setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
 		res.send("Özgür ÖZALP");
 	});
 	app.get("/client-count", (req, res) => {
