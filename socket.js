45071e2191b0d354d50d188f6007615b51166af8
Özgür ÖZALP <ozgurozalp1999@gmail.com>
2019-12-21 16:28:03 +0300

    room eklendi

--- socket.js
+++ socket.js
@@ -7,21 +7,25 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 const allClients = [];
+const rooms = {};
 
 io.on("connection", socket => {
 
 	allClients.push({
 		id : socket.id,
-		userName : null
+		userName : null,
+		roomId : null
 	});
 
-	socket.on("add-user", userName => {
+	socket.on("add-user", data => {
 		let indexNo = allClients.findIndex(client => client.id == socket.id);
 		if (indexNo > -1) {
-			allClients[indexNo].userName = userName;
+			allClients[indexNo].userName = data.userName;
+			allClients[indexNo].roomId = data.codeId;
 			socket.emit("welcome", {
 				userName,
-				row : indexNo + 1
+				row : indexNo + 1,
+				info : allClients[indexNo]
 			});
 		}
 	});
@@ -42,12 +46,14 @@ io.on("connection", socket => {
 
 	socket.on("disconnect", () => {
 		let indexNo = allClients.findIndex(client => client.id == socket.id);
-		if (indexNo > -1) allClients.splice(indexNo, 1);
+		if (indexNo > -1)
+			allClients.splice(indexNo, 1);
 	});
 
 	app.get("/", (req, res) => {
 		res.send("Özgür ÖZALP");
 	});
+
 	app.get("/client-count", (req, res) => {
 		res.json({
 			count: socket.client.conn.server.clientsCount
