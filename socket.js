e1a2cee2abac00082ad970a0fa1f9cf750330272
Özgür ÖZALP <ozgurozalp1999@gmail.com>
2019-12-28 16:15:01 +0300

    change db

--- socket.js
+++ socket.js
@@ -1,11 +1,13 @@
-const app = require("express")();
-const cors = require("cors");
-const mysql = require("mysql");
-const server = require("http").Server(app);
+// Gerekli kütüphaneler
+const app = require("express")(); // http sunucusu için
+const cors = require("cors"); // siteler arası veri transferi için izin
+const mysql = require("mysql"); // mysql için
+const server = require("http").Server(app); // sunucuyu kurma
 const io = require("socket.io")(server);
 const fetch = require('node-fetch');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; // sunucunun hangi portta çalışacağı
 
+// veritabanına bağlanmak için sunucu bilgilerini girdik
 const db = mysql.createConnection({
 	host: "remotemysql.com",
 	user: "R8nGjvFVRF",
@@ -13,44 +15,70 @@ const db = mysql.createConnection({
 	database: "R8nGjvFVRF"
 });
 
-db.connect(err => {
-	if (err) return console.log(err);
-	console.log("Connected..");
-});
 
+// cors için kullanıma açma
 app.use(cors());
 
+
+// ilaç sorgulama
 app.get('/eczane/:ad', (req, res) => {
-	let sql = `SELECT eczane.eczane_ad, ilac.ilac_ad, eczane.eczane_adres, eczane.eczane_tel_no, tablo_stok.adet FROM tablo_stok INNER JOIN eczane on eczane.eczane_id = tablo_stok.eczane_id INNER JOIN ilac on tablo_stok.ilac_id = ilac.ilac_id WHERE ilac.ilac_ad LIKE '%${req.params.ad}%'`;
+	// veritabanına bağlantıyı başlatıyor
+	db.connect(err => {
+		if (err) console.log(err);
+		console.log("Connected..");
+	});
+
+	let sql = `SELECT eczane.eczane_ad, ilac.ilac_ad, eczane.eczane_adres, eczane.eczane_tel_no, 
+		tablo_stok.adet FROM tablo_stok INNER JOIN eczane on eczane.eczane_id = tablo_stok.eczane_id 
+		INNER JOIN ilac on tablo_stok.ilac_id = ilac.ilac_id WHERE ilac.ilac_ad LIKE '%${req.params.ad}%'`;
+
 	db.query(sql, (error, result, fields) => {
 		if (error) return console.log(error);
 		res.json(result);
 	});
+	db.destroy();
 
 });
 
+//hizir.ozgurozalp.com/register/ashjkjadda/sdshjkahda
+// Kayıt olma
 app.get('/register/:email/:pass', (req, res) => {
+	// veritabanına bağlantıyı başlatıyor
+	db.connect(err => {
+		if (err) console.log(err);
+		console.log("Connected..");
+	});
+
 	let {email, pass} = req.params;
 	let sql = `INSERT INTO kullanici (kullanici_email, kullanici_sifre) VALUES ('${email}', '${pass}')`;
 	db.query(sql, (err, result) => {
 		if (err) return console.log(err);
 		res.json({status : true});
 	});
+	db.destroy();
 });
+
+// giriş yapma
 app.get('/login/:email/:pass', (req, res) => {
+	// veritabanına bağlantıyı başlatıyor
+	db.connect(err => {
+		if (err) console.log(err);
+		console.log("Connected..");
+	});
+
 	let {email, pass} = req.params;
 	let sql = `SELECT kullanici_email, kullanici_sifre from kullanici WHERE kullanici_email = '${email}' AND kullanici_sifre = '${pass}'`;
 	db.query(sql, (err, result) => {
-
 		if (err) return console.log(err);
 		if (result.length > 0)
 			res.json({status : true});
 		else
 			res.json({status : false});
 	});
+	db.destroy();
 });
 
-
+//
 
 const allClients = [];
 
@@ -127,4 +155,4 @@ io.on("connection", socket => {
 	});
 });
 
-server.listen(PORT, () => console.log(`${PORT} portunda çalışıyorum.`));
+server.listen(PORT, () => console.log(`${PORT} portunda çalışıyorum.`)); // sunucuyu ayağa kaldırıyor
